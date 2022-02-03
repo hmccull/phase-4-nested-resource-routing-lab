7aece416b357380f651261168b134347dfb341a8
Hannah McCullough <hmac091@gmail.com>
2022-02-02 18:10:29 -0700

    commit tree c99311a6a0bddaeb8121d35789bb443302f648e1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,46 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
+  # GET /users/:user_id/items
   def index
-    items = Item.all
+    if params[:user_id]
+      # need to refactor
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  # GET /users/:user_id/items/:id
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  #  POST /users/:user_id/items
+  def create
+    # need to refactor
+    user = User.find(params[:user_id])
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+  end
+
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response(invalid)
+    render json: { errors: "Request not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
